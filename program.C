fdd6ed465098ef534a47cdeb5ad640d60805e77c
192411020simats-code <192411020.simats@saveetha.com>
2026-08-18 10:27:01 +0530

    Update program.C

--- program.C
+++ program.C
@@ -1,1 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main()
+{
+    printf("Process ID: %d\n", getpid());
+    printf("Parent Process ID: %d\n", getppid());
+
+    fork();   // Create a new process
+
+    printf("After fork\n");
+    printf("Process ID: %d\n", getpid());
+    printf("Parent Process ID: %d\n", getppid());
+
+    return 0;
+}
 
